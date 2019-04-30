71c0691bfcfc7c34d439d3ca02e263c5ece72e5a
Nishanth Arepalli <neesh.bigdata@gmail.com>
2019-04-30 15:12:54 -0500

    Update run.sh change port to 8000

--- jenkins-with-docker/run.sh
+++ jenkins-with-docker/run.sh
@@ -4,7 +4,7 @@ cd
 docker run --name jenkins-docker \
 	-d \
 	--rm \
-	-p 8080:8080 \
+	-p 8000:8080 \
 	-v jenkins_home:/var/jenkins_home \
 	-v /var/run/docker.sock:/var/run/docker.sock \
 	jenkins-with-docker
